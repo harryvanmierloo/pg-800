4e17df378ce195a8ba40bb475cd6edc0c494c978
Harry van Mierloo <info@ontwerper.com>
2020-03-19 17:25:46 +0100

    First rewrite to functional component

--- src/components/ParameterSlider/ParameterSlider.js
+++ src/components/ParameterSlider/ParameterSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import {Slider, Tooltip} from '@material-ui/core';
@@ -31,97 +31,22 @@ ValueLabelComponent.propTypes = {
     value: PropTypes.number.isRequired,
 };
 
-export default class ParameterSlider extends React.Component {
-    constructor(props) {
-        super(props);
+function ParameterSlider(props) {
 
-        const param = MKS.parameters[props.parameter];
+    const parameterId = props.parameter;
+    const parameter = MKS.parameters[parameterId];
+    const label = (parameter.label !== undefined) ? parameter.label : parameter.name,
+          marks = parameter.marks,
+          step = parameter.marks ? null : 1,
+          max = (parameter.marks !== undefined) ? parameter.marks[parameter.marks.length-1].value : parameter.max,
+          min = parameter.min;
 
-        const label = (param.label !== undefined) ? param.label : param.name,
-              marks = param.marks,
-              step = param.marks ? null : 1,
-              max = (param.marks !== undefined) ? param.marks[param.marks.length-1].value : param.max;
+    const [value, setValue] = useState(props.value);
 
-        this.state = {
-            parameter: props.parameter,
-            label: label,
-            name: param.name,
-            marks: marks,
-            step: step,
-            min: param.min,
-            max: max,
-            value: props.value
-        };
-    }
+    const changeHandler = (event, newValue) => {
 
-    changeHandler = (event, newValue) => {
-        
-        if (newValue !== this.state.value) {
-            this.props.onChange(this.state.parameter, newValue);
-            MKS.midiOut.sendSysex(
-                0b01000001, // Roland ID
-                [
-                    0b00110110, // Operation code = IPR (individual parameter)
-                    MKS.midiControlChannel-1, // Control Channel (Start at 0)
-                    0b00100100, // Format type (JX-10)
-                    0b00100000, // Level = 1
-                    0b00000001, // Group (01 = Tone A, 10 = Tone B)
-                    this.state.parameter, // Parameter (0-68)
-                    newValue, // Value (0-127)
-                ]
-            );
-        }
-    }
-
-    render() {
-
-        const container = {
-            height: 60,
-            marginTop: ".5rem",
-        }
-
-        return (
-            <React.Fragment>
-                <Typography variant="caption" gutterBottom>{this.state.label}</Typography>
-                <div style={container}>
-                    <Slider
-                        ValueLabelComponent={ValueLabelComponent}
-                        value={this.props.value}
-                        min={this.state.min}
-                        max={this.state.max}
-                        marks={this.state.marks}
-                        step={this.state.step}
-                        orientation="vertical"
-                        ValueLabelComponent={ValueLabelComponent}
-                        onChange={this.changeHandler}
-                    />
-                </div>
-            </React.Fragment>
-        )
-    }
-}
-
-const OldSlider = props => {
-
-    const classes = useStyles();
-    
-    const sliderRef = useRef();
-    
-    MKS.parameters[props.id].slider = sliderRef;
-
-    const [value, setValue] = React.useState(30)
-    const [param, setParam] = React.useState(MKS.parameters[props.id]);
-
-    const defaultValue = param.defaultValue ? param.defaultValue : 64;
-    
-    const label = (param.label !== undefined) ? param.label : param.name;
-    const marks = param.marks;
-    const step = param.marks ? null : 1;
-    const max = (param.marks !== undefined) ? param.marks[param.marks.length-1].value : param.max;
-
-    const handleChange = (event, newValue) => {
         if (newValue !== value) {
-            setValue(newValue);
+            props.onChange(parameterId, newValue);
             MKS.midiOut.sendSysex(
                 0b01000001, // Roland ID
                 [
@@ -130,28 +55,36 @@ const OldSlider = props => {
                     0b00100100, // Format type (JX-10)
                     0b00100000, // Level = 1
                     0b00000001, // Group (01 = Tone A, 10 = Tone B)
-                    props.id, // Parameter (0-68)
+                    parameterId, // Parameter (0-68)
                     newValue, // Value (0-127)
                 ]
             );
         }
-    };
+    }
+
+    const container = {
+        height: 60,
+        marginTop: ".5rem",
+    }
 
     return (
         <React.Fragment>
             <Typography variant="caption" gutterBottom>{label}</Typography>
-            <div className={classes.container}>
+            <div style={container}>
                 <Slider
-                    defaultValue={defaultValue}
-                    min={param.min}
+                    ValueLabelComponent={ValueLabelComponent}
+                    value={props.value}
+                    min={min}
                     max={max}
-                    step={step}
                     marks={marks}
-                    onChange={handleChange}
+                    step={step}
                     orientation="vertical"
                     ValueLabelComponent={ValueLabelComponent}
+                    onChange={changeHandler}
                 />
             </div>
         </React.Fragment>
     )
 }
+
+export default ParameterSlider;
