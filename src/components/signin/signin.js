1ecc8d7800961beff158e56c09dc9cff11466512
Harry van Mierloo <info@ontwerper.com>
2020-05-05 08:39:18 +0200

    Improved sign-in styling

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { Link } from "@reach/router";
 import { auth } from "../../firebase";
+import * as styles from './signin.module.scss';
 
 const SignIn = () => {
     const [email, setEmail] = useState('');
@@ -18,59 +19,57 @@ const SignIn = () => {
     const onChangeHandler = (event) => {
         const {name, value} = event.currentTarget;
 
-        if(name === 'userEmail') {
+        if (name === 'userEmail') {
             setEmail(value);
         }
-        else if(name === 'userPassword'){
+        else if (name === 'userPassword'){
             setPassword(value);
         }
     };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>}
-        <form className="">
-          <label htmlFor="userEmail" className="block">
-            Email:
-          </label>
-          <input
-            type="email"
-            className="my-1 p-1 w-full"
-            name="userEmail"
-            value = {email}
-            placeholder="E.g: faruq123@gmail.com"
-            id="userEmail"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <label htmlFor="userPassword" className="block">
-            Password:
-          </label>
-          <input
-            type="password"
-            className="mt-1 mb-3 p-1 w-full"
-            name="userPassword"
-            value = {password}
-            placeholder="Your Password"
-            id="userPassword"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <button className="bg-green-400 hover:bg-green-500 w-full py-2 text-white" onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
-            Sign in
-          </button>
-        </form>
-        <p className="text-center my-3">
-          Don't have an account?{" "}
-          <Link to="signUp" className="text-blue-500 hover:text-blue-600">
-            Sign up here
-          </Link>{" "}
-          <br />{" "}
-          <Link to = "passwordReset" className="text-blue-500 hover:text-blue-600">
-            Forgot Password?
-          </Link>
-        </p>
-      </div>
+    <div className={styles.signIn}>
+        <h1>Sign In</h1>
+        <div>
+            {error !== null && <div className={styles.error}>{error}</div>}
+            <form className="">
+                <label htmlFor="userEmail" className="block">
+                    Email:
+                </label>
+                <input
+                    type="email"
+                    name="userEmail"
+                    value={email}
+                    placeholder="E.g: faruq123@gmail.com"
+                    id="userEmail"
+                    onChange={(event) => onChangeHandler(event)}
+                />
+                <label htmlFor="userPassword" className="block">
+                    Password:
+                </label>
+                <input
+                    type="password"
+                    name="userPassword"
+                    value={password}
+                    placeholder="Your Password"
+                    id="userPassword"
+                    onChange={(event) => onChangeHandler(event)}
+                />
+                <button onClick={(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+                    Sign in
+                </button>
+            </form>
+            <p>
+                Don't have an account?<br />
+                <Link to="signUp">
+                    Sign up here
+                </Link>{" "}
+                <br />{" "}
+                <Link to="passwordReset">
+                    Forgot Password?
+                </Link>
+            </p>
+        </div>
     </div>
   );
 };
