531d1b39fdc3b955ce029db1ec707d3c22d6f9cb
Harry van Mierloo <info@ontwerper.com>
2020-05-20 09:10:21 +0200

    Improved look & feel of sidebar menu

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -9,8 +9,20 @@ import { auth } from "firebase/app";
 const Sidebar = (props) => {
 
     const user = useContext(UserContext);
+
+    // Ugly hack to get current page
+    const currentPage = window.location.pathname.toUpperCase().substr(1);
     
     const [sidebarVisibility, setSidebarVisibility] = useState(true);
+    const [tab, setTab] = useState(currentPage);
+
+    console.log(currentPage);
+
+    const changeTab = (name) => event => {
+        if (tab !== name) {
+            setTab(name);
+        }
+    };
 
     const toggleSidebarVisibility = event => {
         setSidebarVisibility(!sidebarVisibility);
@@ -22,17 +34,11 @@ const Sidebar = (props) => {
                 <h1>PG-800 Online</h1>
                 <p className={styles.subtitle}>v0.2 - Alpha</p>
 
-                <ul>
-                    <li>
-                        <Link to="/library">Library</Link>
-                    </li>
-                    <li>
-                        <Link to="/editor">Editor</Link>
-                    </li>
-                    <li>
-                        <Link to="/settings">Settings</Link>
-                    </li>
-                </ul>
+                <div className={styles.tabs}>
+                    <Link onClick={changeTab("LIBRARY")} className={tab === "LIBRARY" ? styles.active : null} to="/library">Library</Link>
+                    <Link onClick={changeTab("EDITOR")} className={tab === "EDITOR" ? styles.active : null} to="/editor">Editor</Link>
+                    <Link onClick={changeTab("SETTINGS")} className={tab === "SETTINGS" ? styles.active : null} to="/settings">Settings</Link>
+                </div>
 
                 <footer>
                     {(user) &&
